e31ef2dccccae2ffe09127421cb950496666dfdf
Jakub Musiał <jmusial2003@gmail.com>
2023-05-14 21:19:31 +0200

    welcome_screen refactor

--- app/src/main/java/com/tictactoe_master/MainActivity.kt
+++ app/src/main/java/com/tictactoe_master/MainActivity.kt
@@ -3,53 +3,58 @@ package com.tictactoe_master
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 
-const val MIN_BOARD_SIZE = 3
-const val MAX_BOARD_SIZE = 10
+
 
 class MainActivity : AppCompatActivity() {
-
     private var chosenBoardSize = 3
 
     private lateinit var oneVsOneCV: CardView
-    private lateinit var decreaseSizeBT: TextView
-    private lateinit var increaseSizeBT: TextView
+    private lateinit var decreaseSizeBT: ImageView
+    private lateinit var increaseSizeBT: ImageView
     private lateinit var sizeTV: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        oneVsOneCV = findViewById(R.id.oneVsOneCV)
-        decreaseSizeBT = findViewById(R.id.decreaseSizeBT)
-        increaseSizeBT = findViewById(R.id.increaseSizeBT)
-        sizeTV = findViewById(R.id.sizeTV)
+        this.initView()
+    }
 
-        decreaseSizeBT.text = "<"
-        increaseSizeBT.text = ">"
-        sizeTV.text = chosenBoardSize.toString()
+    private fun initView() {
+        this.oneVsOneCV = findViewById(R.id.one_v_one_cv)
+        this.decreaseSizeBT = findViewById(R.id.decrease_size_iv)
+        this.increaseSizeBT = findViewById(R.id.increase_size_iv)
+        this.sizeTV = findViewById(R.id.sizeTV)
 
-        oneVsOneCV.setOnClickListener {
+        this.sizeTV.text = this.chosenBoardSize.toString()
+
+        this.oneVsOneCV.setOnClickListener {
             val startGameIntent = Intent(this, GameActivity::class.java)
-            startGameIntent.putExtra("size", chosenBoardSize)
+            startGameIntent.putExtra("size", this.chosenBoardSize)
             startActivity(startGameIntent)
         }
 
-        decreaseSizeBT.setOnClickListener {
-            if (chosenBoardSize > MIN_BOARD_SIZE) {
-                chosenBoardSize--
-                sizeTV.text = chosenBoardSize.toString()
+        this.decreaseSizeBT.setOnClickListener {
+            if (this.chosenBoardSize > MainActivity.MIN_BOARD_SIZE) {
+                this.chosenBoardSize--
+                this.sizeTV.text = this.chosenBoardSize.toString()
             }
         }
 
-        increaseSizeBT.setOnClickListener {
-            if (chosenBoardSize < MAX_BOARD_SIZE) {
-                chosenBoardSize++
-                sizeTV.text = chosenBoardSize.toString()
+        this.increaseSizeBT.setOnClickListener {
+            if (this.chosenBoardSize < MainActivity.MAX_BOARD_SIZE) {
+                this.chosenBoardSize++
+                this.sizeTV.text = this.chosenBoardSize.toString()
             }
         }
     }
 
+    companion object {
+        const val MIN_BOARD_SIZE = 3
+        const val MAX_BOARD_SIZE = 10
+    }
 }
