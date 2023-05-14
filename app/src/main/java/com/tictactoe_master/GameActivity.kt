4e769a44dd5c97b453c058d07b85e7c17c4bce43
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2023-05-14 21:22:15 +0200

    Welcome screen (#4)
    Authors: Mikolaj Krupinski, Jakub Musiał
    
    Init welcome screen activite + layout

---
+++ app/src/main/java/com/tictactoe_master/GameActivity.kt
@@ -0,0 +1,64 @@
+package com.tictactoe_master
+
+import android.graphics.Color
+import android.graphics.Typeface
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class GameActivity : AppCompatActivity() {
+
+    private var size = 3
+
+    private lateinit var gameBoardTL: TableLayout
+    private lateinit var cells: Array<Array<TextView>>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+        gameBoardTL = findViewById(R.id.gameBoardTL)
+        size = intent.getIntExtra("size", 3)
+        placeSquares()
+    }
+
+    private fun placeSquares() {
+        gameBoardTL.removeAllViews()
+        cells = Array(size) { Array(size) { TextView(this) } }
+        for (i in 0 until size) {
+            val tableRow = TableRow(this)
+            for (j in 0 until size) {
+                val layoutParams = TableRow.LayoutParams(
+                    0,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    1f
+                )
+                layoutParams.setMargins(3, 3, 3, 3)
+                cells[i][j].layoutParams = layoutParams
+                cells[i][j].textSize = 22f
+                cells[i][j].setTypeface(null, Typeface.BOLD)
+                cells[i][j].setBackgroundColor(Color.LTGRAY)
+                cells[i][j].gravity = Gravity.CENTER
+                cells[i][j].setOnClickListener { cellClick(cells[i][j], i, j) }
+                tableRow.addView(cells[i][j])
+            }
+            val rowParams = TableLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                0,
+                1f
+            )
+            tableRow.layoutParams = rowParams
+            gameBoardTL.addView(tableRow)
+        }
+    }
+
+    private fun cellClick(textView: TextView, i: Int, j: Int) {
+        Toast.makeText(this, "$i $j clicked", Toast.LENGTH_SHORT).show()
+    }
+
+}
--- app/src/main/java/com/tictactoe_master/MainActivity.kt
+++ app/src/main/java/com/tictactoe_master/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.tictactoe_master
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+
+
 
 class MainActivity : AppCompatActivity() {
+    private var chosenBoardSize = 3
+
+    private lateinit var oneVsOneCV: CardView
+    private lateinit var decreaseSizeBT: ImageView
+    private lateinit var increaseSizeBT: ImageView
+    private lateinit var sizeTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.initView()
+    }
+
+    private fun initView() {
+        this.oneVsOneCV = findViewById(R.id.one_v_one_cv)
+        this.decreaseSizeBT = findViewById(R.id.decrease_size_iv)
+        this.increaseSizeBT = findViewById(R.id.increase_size_iv)
+        this.sizeTV = findViewById(R.id.sizeTV)
+
+        this.sizeTV.text = this.chosenBoardSize.toString()
+
+        this.oneVsOneCV.setOnClickListener {
+            val startGameIntent = Intent(this, GameActivity::class.java)
+            startGameIntent.putExtra("size", this.chosenBoardSize)
+            startActivity(startGameIntent)
+        }
+
+        this.decreaseSizeBT.setOnClickListener {
+            if (this.chosenBoardSize > MainActivity.MIN_BOARD_SIZE) {
+                this.chosenBoardSize--
+                this.sizeTV.text = this.chosenBoardSize.toString()
+            }
+        }
+
+        this.increaseSizeBT.setOnClickListener {
+            if (this.chosenBoardSize < MainActivity.MAX_BOARD_SIZE) {
+                this.chosenBoardSize++
+                this.sizeTV.text = this.chosenBoardSize.toString()
+            }
+        }
+    }
+
+    companion object {
+        const val MIN_BOARD_SIZE = 3
+        const val MAX_BOARD_SIZE = 10
     }
 }
