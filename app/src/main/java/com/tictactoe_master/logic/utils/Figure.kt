cd5973137ba3791a90bf1db41d1438dd4693336c
Zofia Wiora <zofia.wiora@gmail.com>
2023-05-27 22:58:44 +0200

    Prepared setters

--- app/src/main/java/com/tictactoe_master/logic/utils/Figure.kt
+++ app/src/main/java/com/tictactoe_master/logic/utils/Figure.kt
@@ -10,24 +10,30 @@ enum class Figure {
         override fun getImageResource(): Int{
             return android.R.color.transparent
         }
+        override fun setImageResource(res: Int) {}
     },
 
     O {
+        private var res: Int = R.drawable.circle
         override fun next(): Figure = X
         override fun toString(): String = "O"
-        override fun getImageResource(): Int{
-            return R.drawable.circle
+        override fun getImageResource() = res
+        override fun setImageResource(res: Int) {
+            this.res = res
         }
     },
 
     X {
+        private var res: Int = R.drawable.cross
         override fun next(): Figure = O
         override fun toString(): String = "X"
-        override fun getImageResource(): Int{
-            return R.drawable.cross
+        override fun getImageResource() = res
+        override fun setImageResource(res: Int) {
+            this.res = res
         }
     };
 
     abstract fun next(): Figure
     abstract fun getImageResource(): Int
+    abstract fun setImageResource(res: Int)
 }
