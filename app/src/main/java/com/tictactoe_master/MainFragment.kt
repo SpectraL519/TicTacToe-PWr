4b27535bf71fb5be184b420b688698c248e4f417
Zofia Wiora <zofia.wiora@gmail.com>
2023-05-28 18:38:48 +0200

    Main fragment logic

--- app/src/main/java/com/tictactoe_master/MainFragment.kt
+++ app/src/main/java/com/tictactoe_master/MainFragment.kt
@@ -1,59 +1,139 @@
 package com.tictactoe_master
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.tictactoe_master.activity.ChooseGameTypeActivity
+import com.tictactoe_master.activity.GalleryActivity
+import com.tictactoe_master.activity.LoginActivity
+import com.tictactoe_master.app_data.CoinHandler
+import com.tictactoe_master.app_data.FileDataHandler
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var oneVsOneCV: CardView
+    private lateinit var oneVsBotCV: CardView
+    private lateinit var oneVsOneOnlineCV: CardView
+    private lateinit var shopCV: CardView
+    private lateinit var accountTV: TextView
+    private lateinit var coinsTV: TextView
+    private var saveFileLoaded = false
+
+    private lateinit var myView : View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        myView =inflater.inflate(R.layout.fragment_main, container, false)
+
+        if (!saveFileLoaded) {
+            CoinHandler.loadBalance(activity as AppCompatActivity)
+            saveFileLoaded = true
+        }
+
+        this.initPrices()
+        this.initView()
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return myView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        super.onStart()
+
+        this.setAccountTVText()
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        coinsTV.text = String.format(
+            "%s %s",
+            CoinHandler.getBalance(),
+            getText(R.string.currency)
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        CoinHandler.saveBalance(activity as AppCompatActivity)
+    }
+
+    private fun setAccountTVText() {
+        this.accountTV.text = when (Firebase.auth.currentUser) {
+            null -> getString(R.string.login)
+            else -> "${getString(R.string.sign_out)} (${Firebase.auth.currentUser!!.email!!.subSequence(0, 5)})"
+        }
+    }
+
+    private fun initView() {
+        this.oneVsOneCV = myView.findViewById(R.id.one_v_one_cv)
+        this.oneVsBotCV = myView.findViewById(R.id.one_v_bot_cv)
+        this.oneVsOneOnlineCV = myView.findViewById(R.id.one_v_one_online_cv)
+        this.shopCV = myView.findViewById(R.id.shop_cv)
+        this.accountTV = myView.findViewById(R.id.account_tv)
+        this.coinsTV = myView.findViewById(R.id.coins_tv)
+
+        this.setAccountTVText()
+        this.accountTV.setOnClickListener {
+            if (Firebase.auth.currentUser == null) {
+                val loginIntent = Intent(activity, LoginActivity::class.java)
+                startActivity(loginIntent)
+            }
+            else {
+                Firebase.auth.signOut()
+                this.accountTV.text = getString(R.string.login)
+                Toast.makeText(activity, "You've been signed out", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        this.oneVsOneCV.setOnClickListener {
+            this.startGame("1_v_1")
+        }
+
+        this.oneVsBotCV.setOnClickListener {
+            this.startGame("1_v_bot")
+        }
+
+        this.oneVsOneOnlineCV.setOnClickListener {
+            if (Firebase.auth.currentUser == null)
+                Toast.makeText(activity, "First log in to play online", Toast.LENGTH_SHORT).show()
+            else
+                this.startGame("1_v_1_online")
+        }
+
+        this.shopCV.setOnClickListener {
+            val myIntent = Intent(activity, GalleryActivity::class.java)
+            startActivity(myIntent)
+        }
+    }
+
+    private fun startGame (gameMode: String) {
+        val gameIntent = Intent(activity, ChooseGameTypeActivity::class.java).apply {
+            putExtra("game_mode", gameMode)
+        }
+        startActivity(gameIntent)
+    }
+
+    private fun initPrices(){
+        if(!FileDataHandler.checkInt(activity as AppCompatActivity, "p0")){
+            FileDataHandler.writeInt(activity as AppCompatActivity, "img1", R.drawable.circle)
+            FileDataHandler.writeInt(activity as AppCompatActivity, "img2", R.drawable.cross)
+            FileDataHandler.writeInt(activity as AppCompatActivity, "p0", 0)
+            FileDataHandler.writeInt(activity as AppCompatActivity, "p1", 0)
+            FileDataHandler.writeInt(activity as AppCompatActivity, "p2", 20)
+            FileDataHandler.writeInt(activity as AppCompatActivity, "p3", 20)
+            FileDataHandler.writeInt(activity as AppCompatActivity, "p4", 20)
+        }
+    }
+
 }
--- app/src/main/java/com/tictactoe_master/activity/MainActivity.kt
+++ app/src/main/java/com/tictactoe_master/activity/MainActivity.kt
@@ -3,7 +3,6 @@ package com.tictactoe_master.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
@@ -12,116 +11,10 @@ import com.google.firebase.ktx.Firebase
 import com.tictactoe_master.R
 import com.tictactoe_master.app_data.CoinHandler
 import com.tictactoe_master.app_data.FileDataHandler
-import com.tictactoe_master.logic.utils.Figure
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var oneVsOneCV: CardView
-    private lateinit var oneVsBotCV: CardView
-    private lateinit var oneVsOneOnlineCV: CardView
-    private lateinit var shopCV: CardView
-    private lateinit var accountTV: TextView
-    private lateinit var coinsTV: TextView
-    private var saveFileLoaded = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (!saveFileLoaded) {
-            CoinHandler.loadBalance(this)
-            saveFileLoaded = true
-        }
-
-        this.initPrices()
-        this.initView()
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        this.setAccountTVText()
-    }
-
-
-    override fun onResume() {
-        super.onResume()
-        coinsTV.text = String.format(
-            "%s %s",
-            CoinHandler.getBalance(),
-            getText(R.string.currency)
-        )
-    }
-
-    override fun onStop() {
-        super.onStop()
-        CoinHandler.saveBalance(this)
-    }
-
-    private fun setAccountTVText() {
-        this.accountTV.text = when (Firebase.auth.currentUser) {
-            null -> getString(R.string.login)
-            else -> "${getString(R.string.sign_out)} (${Firebase.auth.currentUser!!.email!!.subSequence(0, 5)})"
-        }
-    }
-
-    private fun initView() {
-        this.oneVsOneCV = findViewById(R.id.one_v_one_cv)
-        this.oneVsBotCV = findViewById(R.id.one_v_bot_cv)
-        this.oneVsOneOnlineCV = findViewById(R.id.one_v_one_online_cv)
-        this.shopCV = findViewById(R.id.shop_cv)
-        this.accountTV = findViewById(R.id.account_tv)
-        this.coinsTV = findViewById(R.id.coins_tv)
-
-        this.setAccountTVText()
-        this.accountTV.setOnClickListener {
-            if (Firebase.auth.currentUser == null) {
-                val loginIntent = Intent(this, LoginActivity::class.java)
-                startActivity(loginIntent)
-            }
-            else {
-                Firebase.auth.signOut()
-                this.accountTV.text = getString(R.string.login)
-                Toast.makeText(this, "You've been signed out", Toast.LENGTH_LONG).show()
-            }
-        }
-
-        this.oneVsOneCV.setOnClickListener {
-            this.startGame("1_v_1")
-        }
-
-        this.oneVsBotCV.setOnClickListener {
-            this.startGame("1_v_bot")
-        }
-
-        this.oneVsOneOnlineCV.setOnClickListener {
-            if (Firebase.auth.currentUser == null)
-                Toast.makeText(this, "First log in to play online", Toast.LENGTH_SHORT).show()
-            else
-                this.startGame("1_v_1_online")
-        }
-
-        this.shopCV.setOnClickListener {
-            val myIntent = Intent(this, GalleryActivity::class.java)
-            startActivity(myIntent)
-        }
-    }
-
-    private fun startGame (gameMode: String) {
-        val gameIntent = Intent(this, ChooseGameTypeActivity::class.java).apply {
-            putExtra("game_mode", gameMode)
-        }
-        startActivity(gameIntent)
-    }
-
-    private fun initPrices(){
-        if(!FileDataHandler.checkInt(this, "p0")){
-            FileDataHandler.writeInt(this, "img1", R.drawable.circle)
-            FileDataHandler.writeInt(this, "img2", R.drawable.cross)
-            FileDataHandler.writeInt(this, "p0", 0)
-            FileDataHandler.writeInt(this, "p1", 0)
-            FileDataHandler.writeInt(this, "p2", 20)
-            FileDataHandler.writeInt(this, "p3", 20)
-            FileDataHandler.writeInt(this, "p4", 20)
-        }
     }
 }
